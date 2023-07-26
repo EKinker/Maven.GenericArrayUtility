0746acba9a4d06449728c9655dec8673d4503c97
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-26 19:47:58 -0400

    All tests passed

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.arrayutility;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * Created by leon on 3/6/18.
@@ -15,13 +16,35 @@ public class ArrayUtility<T> {
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        T[] newArray = mergeArray(arrayToMerge);
+        int counter = 0;
+        for (T t : newArray){
+            if (t.equals(valueToEvaluate)){
+                counter++;
+            }
+        }
+        return counter;
+    }
 
-
-        return null;
+    private T[] mergeArray(T[] arrayToMerge) {
+        T[] newArray = Arrays.copyOf(inputArray, inputArray.length+arrayToMerge.length);
+        System.arraycopy(arrayToMerge, 0, newArray,inputArray.length,arrayToMerge.length);
+        return newArray;
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
+        T[]  newArray = mergeArray(arrayToMerge);
+        int maxInt = -1;
+        T thing = null;
+        for (T t: newArray){
+            if (getNumberOfOccurrences(t)>maxInt){
+                maxInt = getNumberOfOccurrences(t);
+                thing = t;
+            }
+        }
+        
+        
+        return thing;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
